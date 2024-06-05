24b5dc4945c284d2e92227ae8ea5a1607cb1f2d9
Yoshidamouth <gomanosuke0507@gmail.com>
2024-06-05 15:50:03 +0900

    commentsコントローラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments
       render "prototypes/show", status: :unprocessable_entity
     end
   end
